b2924b4b3bdbe9de37679590bc5ef3eb28d9965d
oSaskia <b.t@posteo.de>
2024-05-20 20:51:09 +0200

    Add files via upload

---
+++ content.js
@@ -0,0 +1,101 @@
+// Popup 2024-05-19
+var content_20240519 = `
+<div class="popup-content">
+    <video controls>
+        <source src="./media/2024-05-19.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+    <h3>Spreewald</h3>
+    <div class="date">19.05.2024</div>
+    <div class="scrollable-text">
+<p>Grüßt euch!</p>
+<p>Mittlerweile liegt der dritte Wandertag bereits hinter mir. Nachdem ich es am Freitag nach etwa 25 Kilometer zumindest schon Mal über die Berliner Stadtgrenze geschafft hatte stand dann gestern eine größere Etappe an. Da mich die Brandenburger Sonne und die Flugzeuge des BER bereits um 6 Uhr morgens wachgeküsst hatten ging's eine halbe Stunde später direkt los. Erstmal lange entlang des Bahndamms des RE2 südlich Richtung Cottbus.</p>
+<p>Kurz vor Bestensee bekam ich dann das große Zittern. Und das bekam ich - weniger aus Angst vor dem langen Weg der noch vor mir liegt - sondern eher aus Unterzuckerung. Vielleicht beidem. Immerhin war ich nach einem üppigen Frühstück bis in den Nachmittag hineingelaufen und hatte dabei wohl zu wenig gegessen. Eine Nudelpfanne mit Cola haben das aber zügig wieder gerichtet.</p>
+<p>Dennoch wird mich das Thema wohl noch länger beschäftigen. Diverse Rechner im Web sagen mir mit, dass ich bei meinen durchschnittlichen Tagesetappen rund 2000 Kilokalorien zusätzlich zu meinem etwa ebenso großen Grundumsatz verbrauche. Das heißt ich müsste pro Tag etwa sechs Döner (650 kcal) essen müsste, um nicht sehr schnell mein kostbares Körpergewicht zu verlieren.</p>
+<p>Ich denke, ich werde kräftig Mandeln zufüttern (200g = 1000kcal) oder die ein oder andere Flasche Flüssignahrung zu mir nehmen. Aber jetzt gilt es erstmal das lange Wochenende ohne Kraftfutter zu überstehen.</p>
+<p>Aber zurück zur Route: Von Bestensee gings's weiter gen Süden nach Groß und Klein Köris. Hier war es dann endlich soweit: Ich hatte den Europäischen Fernwanderweg E10 erreicht, der mich - Inshallah - bis nach Österreich bringen soll.</p>
+<p>Hier stand ich dann vor der Wahl ob ich es nach den bisherigen 20km Mal gut sein lassen sollte oder ob ich nochmal 12 draufpacke und durch den Wald nach Märkisch Buchholz weiterlaufe. Es war noch früh, so ging ich los. <br>Und das war dann vielleicht ein bisschen viel für den zweiten Tag.</p>
+<p>Kurz für die Kenner des Fachs: Mein Rucksack wiegt trocken, ohne Essen und Wasser etwa 13kg. Mit Proviant so 17kg.</p>
+<p>Der zog mich dann gegen Abend immer kräftiger in Richtung des sandigen Waldbodens. Aber mit vielen Pausen im Nieselregen ging's dann doch. Und ich glaube bleibenden Schaden hat der Laufapparat dabei nicht genommen.</p>
+<p>In der zweiten Nacht war dann der Schlaf entsprechend etwas tiefer und länger und die heutige Etappe etwas kürzer. Nach gut 16km bin ich mit schlurfendem Schritt im Spreewaldnest Schlepzig angekommen. Abgesehen von ein paar Wehwehchen (unappetitliche Hautirritationen auf den Schultern vom Rucksack, ein paar kleinen Druckstellen am Fuß) geht's mir tiptop.</p>
+<p>Die Landschaft hat sich von sandigen Kiefernwäldern mittlerweile in die sumpfigen Mangroven des Spreewaldes gewandelt. Aus dessen morastigen Sümpfen steigen neben modrigen Gasen, die die Sinne zu vernebeln scheinen, auch den ganzen Tag unablässig Mückenschwärme auf und suchen nach Beute. Das macht aber nix.</p>
+<p>Gerade donnert es ganz hübsch, aber jetzt ist es ohnehin Zeit fürs Gasthaus.</p>
+<p>Grüße! :)</p>
+<p>(Nochmal für die Kenner: Für die Messung der Distanzen benutze die Schrittzähler App von Google, die ist recht zuverlässig.)</p>
+<!-- Füge hier mehr Text hinzu -->
+</div>
+</div>
+`;
+
+var marker_20240519 = L.marker([52.05175, 13.855472]).addTo(mymap).bindPopup(content_20240519).closePopup();
+
+// Popup 2024-05-19-2
+var content_202405192 = `
+<div class="popup-content">
+    <img src="./media/2024-05-19-2.jpg" alt="Foto" class="popup-image">
+    <h3>Schlafplatz</h3>
+    <div class="date">19.05.2024</div>
+    <div class="scrollable-text">
+<p>extrem viele Mücken aber schöne Aussicht</p>
+<!-- Füge hier mehr Text hinzu -->
+</div>
+</div>
+`;
+
+var marker_202405192 = L.marker([52.014028, 13.885306]).addTo(mymap).bindPopup(content_202405192).closePopup();
+
+// Popup 2024-05-18
+var content_20240518 = `
+<div class="popup-content">
+    <img src="./media/2024-05-18.jpg" alt="Foto" class="popup-image">
+    <h3>Schlafplatz</h3>
+    <div class="date">18.05.2024</div>
+    <div class="scrollable-text">
+<p>Biwakplatz mit Dusche</p>
+<!-- Füge hier mehr Text hinzu -->
+</div>
+</div>
+`;
+
+var marker_20240518 = L.marker([52.112028, 13.762333]).addTo(mymap).bindPopup(content_20240518).closePopup();
+
+// Popup 2024-05-17-2
+var content_202405172 = `
+<div class="popup-content">
+    <video controls>
+        <source src="./media/2024-05-17-2.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+    <h3>Der erste Schlafplatz</h3>
+    <div class="date">17.05.2024</div>
+    <div class="scrollable-text">
+<p>Bisher bin ich noch weiterstehend in bekanntestem Terrain gelaufen. Damit ist es dann aber morgen vorbei. Heute schlage ich mein Zelt am Ufer der Dahme knapp südlich von Wildau auf. Kurz werde ich noch versuchen die Mückenflut mit dem Geruch von Zigaretten und Bier zu vertreiben. Wenn mir das nicht gelingt, mich aber bald hinter das schützende Netz verziehen.</p>
+<p>Auf eine ruhige erste Nacht!</p>
+<!-- Füge hier mehr Text hinzu -->
+</div>
+</div>
+`;
+
+var marker_202405172 = L.marker([52.314917, 13.644611]).addTo(mymap).bindPopup(content_202405172).closePopup();
+
+// Popup 2024-05-17-1
+var content_202405171 = `
+<div class="popup-content">
+    <video controls>
+        <source src="./media/2024-05-17-1.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+    <h3>Aufbruch</h3>
+    <div class="date">17.05.2024</div>
+    <div class="scrollable-text">
+<p>So ihr lieben Freunde,</p>
+<p>Hier könnt ihr mich nun auf euren Handys eine Weile beim Wandern begleiten - hoffentlich den ganzen Weg von Berlin bis runter ans Meer!</p>
+<p>Wie oft, wie genau und wie ehrlich ich hier posten werde, weiß ich noch nicht. Aber aber es macht ja nur Spaß, wenn es immer Mal ein bisschen deftig zugeht, gell. Und wahrscheinlich kommen auch viele Videos.</p>
+<p>Jetzt zu den Fakten: Heute um die Mittagsstunde bin ich der Silbersteinstraße in Richtung Südosten aufgebrochen.</p>
+<p>Soweit läuft es sich herrlich. Obwohl ich derzeit noch, im Zustand höchster Sensibilität, jedem kleinsten Wehwehchen in den Gelenken nachspüre, kann ich soweit von keinem Schmerz berichten!</p>
+<!-- Füge hier mehr Text hinzu -->
+</div>
+</div>
+`;
+
+var marker_202405171 = L.marker([52.46650, 13.43371]).addTo(mymap).bindPopup(content_202405171).closePopup();
