c105110f421734fca2334fb539884ea1b732f6bb
oSaskia <b.t@posteo.de>
2024-05-20 21:02:23 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-        var mymap = L.map('map').setView([52.05175, 13.855472], 14);
+        var mymap = L.map('map').setView([52.05175, 13.855472], 12);
 
         var openStreetMapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: "Datenquelle: &copy; <a href='https://www.openstreetmap.org/copyright' target='_blank'>OpenStreetMap</a> contributors",
