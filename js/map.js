a1589a37f1e9455b4f8d2b576483300a9dd01394
oSaskia <b.t@posteo.de>
2024-05-24 12:36:48 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -41,4 +41,16 @@
             showMarker: true, // Zeigt einen Marker für das Suchergebnis an
             showPopup: false, // Deaktiviert das Popup für den Marker
         });
+        // Foto Fullscreen
+        function openFullscreen(element) {
+                if (element.requestFullscreen) {
+                    element.requestFullscreen();
+                } else if (element.mozRequestFullScreen) { /* Firefox */
+                    element.mozRequestFullScreen();
+                } else if (element.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+                    element.webkitRequestFullscreen();
+                } else if (element.msRequestFullscreen) { /* IE/Edge */
+                    element.msRequestFullscreen();
+                }
+            }
         mymap.addControl(search);
